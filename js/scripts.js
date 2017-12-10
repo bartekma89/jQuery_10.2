43277d89e5a79e34954c257d68e9b89de245cf0e
Bartek <bartekma89@gmail.com>
2017-12-10 01:10:09 +0100

    refactored all code

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,105 @@
-$(function () {
+/*$(function () {*/
 
-	var $carouselList = $('#carousel ul');
-	var $next = $('.next');
-	var $prev = $('.prev');
+var $carouselList = $('#carousel ul');
+var $next = $('.next');
+var $prev = $('.prev');
+var slideDirection = {
+	next: 'next',
+	prev: 'prev'
+};
+var $quantityElementsList = $('.pic', $carouselList);
+var indexElement = 0;
+var intervalSlide = 3000;
 
-	$next.click(changeSlidesNext);
-
-	$prev.click(changeSlidesPrev);
-	
-	function changeSlidesPrev() {
-		$carouselList.animate({
-			marginLeft: '0'
-		}, "slow", function () {
-			var $firstSlide = $carouselList.find('li:first');
-			var $lastSlide = $carouselList.find('li:last');
-
-			$firstSlide.before($lastSlide);
-			$carouselList.css({
-				marginLeft: -400
-			});
-		});
+function changeSlide(direction) {
+	if (!$carouselList.is(':animated')) {
+		if (direction === slideDirection.next) {
+			moveNext();
+		} else {
+			movePrev();
+		}
 	}
+}
 
-	//setInterval(changeSlidesNext, 30000);
+function moveNext() {
+	$carouselList.animate({
+		marginLeft: '-=400'
+	}, 'slow', function () {
+		var $firstElement = $carouselList.find('li:first');
+		var $lastElement = $carouselList.find('li:last');
+		$lastElement.after($firstElement);
+		$carouselList.css({
+			marginLeft: 0
+		})
+		counter();
+	})
+}
 
-	function changeSlidesNext() {
-		$carouselList.animate({
-			marginLeft: '-=400'
-		}, "slow", function () {
-			var $firstSlide = $carouselList.find('li:first');
-			var $lastSlide = $carouselList.find('li:last');
+function counter() {
+	indexElement++;
 
-			$lastSlide.after($firstSlide);
-			$carouselList.css({
-				marginLeft: 0
-			});
-		});
+}
 
-	}
+function movePrev() {
+	var $firstElement = $carouselList.find('li:first');
+	var $lastElement = $carouselList.find('li:last');
+	$firstElement.before($lastElement);
+	$carouselList.css('marginLeft', '-=400').animate({
+		marginLeft: 0
+	}, 'slow');
+}
 
+function repeat() {
+	setInterval(function() {
+		changeSlide(slideDirection.next)
+	}, intervalSlide);
+}
+
+repeat();
+
+function stopRepeat() {
+	clearInterval(repeat);
+}
+
+$next.click(function () {
+	stopRepeat();
+	changeSlide(slideDirection.next)
 });
+$prev.click(function () {
+	stopRepeat();
+	changeSlide(slideDirection.prev)
+});
+
+
+
+(function () {
+	var $indicatorList = $('<ol></ol');
+	$indicatorList.addClass('carousel-indicators');
+	$carouselList.after($indicatorList);
+
+	var $documentFragment = $(document.createDocumentFragment());
+	
+	$carouselList.find('li').each(function (index) {
+		var $this = $(this);
+		var $li = $('<li>', {
+			'data-index': index
+		});
+		
+		if ($this.hasClass('pic')) {
+			$documentFragment.append($li);
+		}
+	})
+	
+	$indicatorList.append($documentFragment);
+	
+})();
+
+$('li').click(function(e){
+	console.log(e);
+});
+
+$('ul').click(function(e){
+	console.log(e);
+});
+
+/*});*/
