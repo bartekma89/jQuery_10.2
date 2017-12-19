fcc5dd3a5d1b9b4c233dc7d02a3b72cf7795f6dc
Bartek <bartekma89@gmail.com>
2017-12-19 21:21:29 +0100

    almost done. only add second a carousel

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,20 @@
 (function ($) {
 
-	$.fn.carousel = function () {
+	$.fn.carousel = function (options) {
+
+		var options = $.extend({
+			animateTime: 3000,
+			duration: 'slow',
+			easing: 'swing',
+			auto: false,
+			direction: 'left'
+		}, options);
 
 		return this.each(function () {
 
 			var $carouselList = $('ul', '#carousel');
 			var interval;
-			var intervalSlideTime = 2000;
+			var intervalSlideTime = options.animateTime;
 			var $quantityElementList = $carouselList.find('li').length;
 			var indexElement = 0;
 
@@ -30,36 +38,43 @@
 				var $firstElement = $carouselList.find('li:first');
 				var $lastElement = $carouselList.find('li:last');
 
-				$carouselList.animate({'marginLeft': '-=500'}, 'slow', function () {
+				$carouselList.animate({
+					'marginLeft': '-=500'
+				}, options.duration, options.easing, function () {
 					$lastElement.after($firstElement);
-					$carouselList.css({'marginLeft': 0});
+					$carouselList.css({
+						'marginLeft': 0
+					});
 				});
 			}
 
 			function moveBackwardSlide() {
 				var $firstElement = $carouselList.find('li:first');
 				var $lastElement = $carouselList.find('li:last');
-				
+
 				$firstElement.before($lastElement);
-				$carouselList.css({'marginLeft': '-=500'}).animate({'marginLeft': '0'});
+				$carouselList.css({
+					'marginLeft': '-=500'
+				}).animate({
+					'marginLeft': '0'
+				}, options.duration, options.easing);
 			}
-			
+
 			function counterForward() {
-				if(indexElement < $quantityElementList - 1){
+				if (indexElement < $quantityElementList - 1) {
 					indexElement++;
-				}
-				else
+				} else
 					indexElement = 0;
 			}
-			
+
 			function counterBackward() {
-				if(indexElement === 0) {
+				if (indexElement === 0) {
 					indexElement = $quantityElementList - 1;
 				} else {
 					indexElement--;
 				}
 			}
-			
+
 			function setIndicator() {
 				$('li.active').removeClass('active');
 				$('li', '.carousel-indicators').eq(indexElement).addClass('active');
@@ -71,19 +86,21 @@
 			$('.arrow-prev').click(function () {
 				changeSlide('right');
 			});
-			
+
 			function startRepeat() {
-				interval = setInterval(function(){
-					changeSlide('right');
-				}, intervalSlideTime);
+				if (options.auto) {
+					interval = setInterval(function () {
+						changeSlide(options.direction);
+					}, intervalSlideTime);
+				}
 			}
-			
+
 			function stopRepeat() {
 				clearInterval(interval);
 			}
-			
+
 			startRepeat();
-			
+
 		});
 	}
 
@@ -91,6 +108,9 @@
 
 $(document).ready(function () {
 
-	$('#carousel').carousel();
+	$('#carousel').carousel({
+		auto: true,
+		direction: 'left'
+	});
 
 });
